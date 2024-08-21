d472503d6c86364faa7d5363e94803774cc5b1d3
Daniel Martel <danielmartelmarrero@gmail.com>
2024-08-21 20:59:20 +0100

    shopping cart almost finished

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,8 +3,76 @@ import axios from 'axios'
 
 function CartPage() {
 
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/shoppingcart`)
+      .then((response) => {
+        console.log(response.data)
+        //Sets whatever is inside the shoppincart db json array into the state
+        setCart(response.data)
+      })
+      .catch((err) => { console.log(err) })
+  }, [])
+  console.log(cart)
+
   return (
-    <div>CartPage</div>
+
+    <div id='cartBody'>
+      <h5 id='cartTitle'>Items in your cart</h5>
+      <p id='cartLength'>{cart.length} products</p>
+      <div id='cartMaindiv'>
+        <div>
+          {cart.map(oneProduct => {
+
+            //{setTotal(total=>total+oneProduct.price)}
+            return (
+              <div className='cart-productdiv'>
+                <img className='cart-productpic' src={oneProduct.pictures[0]} alt="" />
+                <div className='cart-proddetails'>
+                  <p>{oneProduct.title.join('')}</p>
+                  <div className='cart-prod-price'>
+                    <p className='cart-price'>{oneProduct.price * oneProduct.quantity}.00€</p>
+                    {oneProduct.originalPrice && <p className='cart-original-price'>{oneProduct.originalPrice * oneProduct.quantity}.00€</p>}
+                  </div>
+                  <p>Price per unit {oneProduct.price}.00€</p>
+                  <div className='cart-prod-buttons'>
+                    <button className='delete-button'>Delete</button>
+                    <div className='cart-productquantity'>
+                      <button>-</button>
+                      <h5>{oneProduct.quantity}</h5>
+                      <button>+</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            )
+          })}
+        </div>
+        <div id='cartResume'>
+          <h5>Resume</h5>
+          <p>You can add your discount voucher during the payment process</p>
+          <hr />
+          <div id='cartSubtotal'>
+            <h6>Subtotal</h6>
+            <h6>2000</h6>
+          </div>
+          <div id='shippingCost'>
+            <h6>Shppping costs</h6>
+            <h6>Calculated in the next step</h6>
+          </div>
+          <hr />
+          <div id='cartTotal'>
+            <h4>Total</h4>
+            <h4>2000</h4>
+          </div>
+          <button>Process payment</button>
+          <button>Continue buying</button>
+        </div>
+      </div>
+    </div>
 
   )
 }
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function ProductDetailsPage() {
 
@@ -19,7 +20,7 @@ function ProductDetailsPage() {
         //Sets whatever is inside the shoppincart db json array into the state
         setCart(response.data)
       })
-      .catch((err)=>{ console.log(err) })
+      .catch((err) => { console.log(err) })
 
     axios.get(`${import.meta.env.VITE_BACKEND_URL}/categories?_embed=Products`)
       .then((response) => {
@@ -27,121 +28,125 @@ function ProductDetailsPage() {
         const foundCategory = response.data.find((oneCategory) => {
           return oneCategory.name == category
         })
-        //Finds the product inside the category stored in line 26
+        //Finds the product inside the category stored in line 27
         const foundProduct = foundCategory.Products.find((oneProduct) => {
           return oneProduct.id == id
         })
         //Saves the product in the state
         setProduct(foundProduct)
       })
-      .catch((err)=>{console.log(err)})
+      .catch((err) => { console.log(err) })
   }, [])
 
-  
- 
+
+
   function addToCart() {
-    
+
     //Check if the product we want to add is already in the cart
-    const existingProd = cart.find(cartItem=> cartItem.id===product.id);
+    const existingProd = cart.find(cartItem => cartItem.id === product.id);
 
 
     //If db json includes our product then...
     if (existingProd) {
       const updatedCart = cart.map(cartItem => {
         //Find the product inside the array stored in line 20 and add value stored in line 9 to the quantity
-        if(cartItem.id===product.id){
-          return {...cartItem, quantity: cartItem.quantity+quantity}
+        if (cartItem.id === product.id) {
+          return { ...cartItem, quantity: cartItem.quantity + quantity }
         }
         //Ignore the other products of the array
-        else{
+        else {
           return cartItem
         }
       })
       //update the db json 
-      axios.put(`${import.meta.env.VITE_BACKEND_URL}/shoppingcart/${existingProd.id}`, { ...existingProd, quantity: existingProd.quantity+quantity})
+      axios.put(`${import.meta.env.VITE_BACKEND_URL}/shoppingcart/${existingProd.id}`, { ...existingProd, quantity: existingProd.quantity + quantity })
         .then((response) => {
           //update the state
           setCart(updatedCart)
         })
-        .catch((err)=>{console.log(err)})
+        .catch((err) => { console.log(err) })
     }
 
 
     //If the db json shoppingcart doesnt iclude the product then...
-    else{
+    else {
       //Add a new key to the product to store the quantity
-      const newProduct ={...product, quantity}
+      const newProduct = { ...product, quantity }
       //Post our newProduct into the db json
       axios.post(`${import.meta.env.VITE_BACKEND_URL}/shoppingcart`, newProduct)
-      //Update the cart state
-      .then((response) => {
-        setCart([...cart, newProduct]);
-      })
-      .catch((err) => { console.log(err) });
+        //Update the cart state
+        .then((response) => {
+          setCart([...cart, newProduct]);
+        })
+        .catch((err) => { console.log(err) });
     }
-
-
-
-
 
     console.log(quantity)
     console.log(cart)
   }
-return (
+  return (
 
-  <div id='productDetPageDiv'>
+    <div id='productDetPageDiv'>
 
-    <p>{category} {'>'} {product?.title?.[0]}</p>
-    <h3>{product?.title?.join('')}</h3>
+      <Link to={`/${category}`}><p className='index2'>{category} {'>'} {product?.title?.[0]}</p></Link>
+      <h3>{product?.title?.join('')}</h3>
 
-    <div id='productMainDiv'>
+      <div id='productMainDiv'>
 
-      <div id='allPicturesDiv'>
-        <img src={product?.pictures?.[0]} alt="" id='mainImg' />
-        <div id='productPictures'>
-          {product?.pictures?.map((onePicture) => {
-            return (
-              <img key={onePicture} className='small-img' src={onePicture}></img>
-            )
-          })}
+        <div id='allPicturesDiv'>
+          <img src={product?.pictures?.[0]} alt="" id='mainImg' />
+          <div id='productPictures'>
+            {product?.pictures?.map((onePicture) => {
+              return (
+                <img key={onePicture} className='small-img' src={onePicture}></img>
+              )
+            })}
+          </div>
         </div>
-      </div>
 
-      <div id='buyDiv'>
-        <div id='priceDiv'>
-          {product.originalPrice && <h5 style={{ textDecoration: 'line-through', margin: '40px 0 0 0' }}>{product?.originalPrice}.-€ </h5>}
-          {product.discPrice ? <h5 className='discPrice'>{product.discPrice}.-€ </h5>: <h5 className='price'>{product.price}.-€ </h5>}
-          
-          <span>Cantidad</span>
-          <select onChange={(e) => { setQuantity(parseInt(e.target.value)) }} name="Cantidad" id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
+        <div id='buyDiv'>
+          <div id='priceDiv'>
+            {product.originalPrice && <h5 style={{ textDecoration: 'line-through', margin: '40px 0 0 0' }}>{product?.originalPrice}.-€ </h5>}
+            {product.discPrice ? <h5 className='discPrice'>{product.discPrice}.-€ </h5> : <h5 className='price'>{product.price}.-€ </h5>}
+
+            <div id="financeDiv">
+              <p><u>Simula tu financiación</u></p>
+              <div>
+                <p>Financiación: 26.22 €/mes</p>
+                <p>TIN 18.95% <strong>TAE 20.63%</strong> en 6 cuotas **</p>
+              </div>
+            </div>
+
+            <span>Cantidad</span>
+            <select onChange={(e) => { setQuantity(parseInt(e.target.value)) }} name="Cantidad" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <button onClick={addToCart} style={{ display: 'block' }}>Buy</button>
         </div>
-        <button onClick={addToCart} style={{ display: 'block' }}>Buy</button>
-      </div>
 
+      </div>
+      <hr style={{ margin: '150px 0' }} />
+      <h4 className='subtitle-detailspg' >Description</h4>
+
+      {product.description && <img id='descriptionPicture' src={product?.description} alt="" />}
+      {product?.descriptionText?.map((oneText, index) => {
+        return (
+          index % 2 == 0 ?
+            (<p style={{ fontSize: '18px' }}>
+              {oneText}
+            </p>) :
+            (<h3>
+              {oneText}
+            </h3>)
+        )
+
+      })}
     </div>
-    <hr style={{ margin: '150px 0' }} />
-    <h4 className='subtitle-detailspg' >Description</h4>
-
-    {product.description && <img id='descriptionPicture' src={product?.description} alt="" />}
-    {product?.descriptionText?.map((oneText, index) => {
-      return (
-        index % 2 == 0 ?
-          (<p style={{ fontSize: '18px' }}>
-            {oneText}
-          </p>) :
-          (<h3>
-            {oneText}
-          </h3>)
-      )
-
-    })}
-  </div>
-)
+  )
 }
 
 export default ProductDetailsPage
